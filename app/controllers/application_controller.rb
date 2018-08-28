b6b7f15a4002b4035648914ddbe09dfa8cc3e4b4
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-28 19:18:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ private
     #client_id = ENV['FOURSQUARE_CLIENT_ID']
     client_id='0W4LE0J2UHWQUYV5EUIGLOK15H1UK2FI23N2JUK00Z4DCXNH'
   #  raise client_id.inspect
-    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    redirect_uri = CGI.escape("http://67.205.165.109.44551/auth")
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
     redirect_to foursquare_url unless logged_in?
   end
